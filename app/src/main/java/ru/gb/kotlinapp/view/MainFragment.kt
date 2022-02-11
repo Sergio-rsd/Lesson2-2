b77a4ffd27f1c2138560d8243c7226c7249310c0
Sergio-rsd <sergsa2001@mail.ru>
2022-02-11 22:20:49 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/kotlinapp/view/MainFragment.kt
@@ -0,0 +1,100 @@
+package ru.gb.kotlinapp.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import ru.gb.kotlinapp.R
+import ru.gb.kotlinapp.databinding.MainFragmentBinding
+import ru.gb.kotlinapp.model.Weather
+import ru.gb.kotlinapp.viewmodel.AppState
+import ru.gb.kotlinapp.viewmodel.MainViewModel
+
+class MainFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val observer = Observer<AppState> {
+            renderData(it)
+        }
+
+        viewModel.getLiveData().observe(viewLifecycleOwner, observer)
+        viewModel.getWeatherFromLocalSource()
+    }
+
+    private fun renderData(appState: AppState) {
+//        val effort = getRandomSuccess()
+//        appState = if (effort) AppState.Success else AppState.Error
+        when (appState) {
+            is AppState.Success -> {
+                val weatherData = appState.weatherData
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(binding.mainView, "Success", Snackbar.LENGTH_LONG)
+                    .show()
+
+                setData(weatherData)
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+
+                Snackbar
+                    .make(binding.mainView, getString(R.string.error), Snackbar.LENGTH_INDEFINITE)
+                    .setAction(getString(R.string.reload)) {
+                        viewModel.getWeatherFromLocalSource()
+                    }
+                    .show()
+            }
+
+        }
+    }
+/*
+
+    private fun getRandomSuccess(): Boolean {
+        return (Math.random() * 2).toInt() != 0
+    }
+*/
+
+    private fun setData(weatherData: Weather) {
+        binding.cityName.text = weatherData.city.city
+        binding.cityCoordinates.text = String.format(
+            getString(R.string.city_coordinates),
+            weatherData.city.lat.toString(),
+            weatherData.city.lon.toString()
+        )
+        binding.temperatureValue.text = weatherData.temperature.toString()
+        binding.feelsLikeValue.text = weatherData.feelsLike.toString()
+    }
+}
---
+++ app/src/main/java/ru/gb/kotlinapp/viewmodel/MainViewModel.kt
@@ -0,0 +1,39 @@
+package ru.gb.kotlinapp.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.gb.kotlinapp.model.Repository
+import ru.gb.kotlinapp.model.RepositoryImpl
+import java.lang.Thread.sleep
+
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: Repository = RepositoryImpl()
+) : ViewModel() {
+
+    fun getLiveData() = liveDataToObserve
+
+    fun getWeatherFromLocalSource() = getDataFromLocalSource()
+    fun getWeatherFromRemoteSource() = getDataFromLocalSource()
+
+    private fun getDataFromLocalSource() {
+        liveDataToObserve.value = AppState.Loading
+
+        Thread {
+            sleep(2000)
+
+            if (getRandomSuccess()) {
+                liveDataToObserve.postValue(AppState.Success(repositoryImpl.getWeatherFromLocalStorage()))
+            } else {
+                liveDataToObserve.postValue(AppState.Error(error = null ))
+            }
+
+        }.start()
+
+    }
+
+    private fun getRandomSuccess(): Boolean {
+        return (Math.random() * 2).toInt() != 0
+    }
+
+}
